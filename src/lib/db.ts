514719dff6a0bdab497244b821c7d70d1ce36feb
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2025-07-14 20:07:41 -0600

    fix fetching db posts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,20 @@
 import postgres from 'postgres';
 
-const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
+const connectionString = import.meta.env.DATABASE_URL!;
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
+const url = new URL(connectionString);
+
+const sql = postgres({
+  host: url.hostname,
+  port: Number(url.port),
+  database: url.pathname.slice(1),
+  username: url.username,
+  password: url.password,
+  ssl: 'require'
+});
 
 export default sql;
